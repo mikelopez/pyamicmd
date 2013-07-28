0a1aac601d9ee9c252db836cae9284beccca2470
Mike Lopez <dev@scidentify.info>
2013-07-28 18:30:39 -0400

    initial import

---
+++ README.rst
@@ -0,0 +1,10 @@
+Readme for pyamicmd
+------------------------------
+
+Small python app based on amiwrapper that sends a command to Asterisk via AMI.
+
+``from pyamicmd import *``
+
+Requirements
+-------------
+Based on starpy and amiwrapper. See requirements.txt for more details
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='pyamicmd',
+      version='0.1',
+      description='Send commands to Asterisk via AMI',
+      author='',
+      license='MIT',
+      py_modules=['pyamicmd'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+          ]
+      )
